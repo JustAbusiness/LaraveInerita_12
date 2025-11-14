948beeac4437e401550d7c3b7c668c4d753b5f50
JustABusiness <huysanti123456@gmail.com>
2025-11-14 21:50:03 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+use Inertia\Inertia;
+use Laravel\Fortify\Features;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Backend\V1\DashboardController;
+
+Route::get('/', function () {
+    return Inertia::render('welcome', [
+        'canRegister' => Features::enabled(Features::registration()),
+    ]);
+})->name('home');
+
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
+});
+ 
+require __DIR__.'/settings.php';
